25979f31dcd457e833f16a0e40d5d8b00fec61d8
Kevin Mendoza Tudares <mendozatudares@u.northwestern.edu>
2022-01-31 21:47:29 -0600

    Complete sample battle screen

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private PlayerControls _pc;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        PlayerControls pc = collision.gameObject.GetComponent<PlayerControls>();
-        if (pc) 
+        _pc = collision.gameObject.GetComponent<PlayerControls>();
+        if (_pc) 
         {
-            pc.Disabled = true;
-            GameObject ui = GameObject.Find("UIManager");
-            ui.GetComponent<UIManager>().StartBattle();
+            _pc.Disabled = true;
+            UIManager ui = GameObject.Find("UIManager").GetComponent<UIManager>();
+            SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+            Action<string> battleAction = m => OnBattleResult(m);
+            ui.StartBattle(sr.sprite, battleAction);
+        }
+    }
+
+    void OnBattleResult(string message)
+    {
+        Debug.Log(message);
+        _pc.Disabled = false;
+        if (message.Equals("victory"))
+        {
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +7,49 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Canvas UICanvas;
-    public Image BattleBackground;
+
+    public Image  BattleBackground;
+    public Image  BattleSprite;
+    public Text   BattleText;
+    public Button BattleAttackButton;
+    public Button BattleFleeButton;
+    private Action<string> _battleAction;
 
     // Start is called before the first frame update
     void Start()
     {
         BattleBackground.enabled = false;
+        BattleSprite.enabled = false;
+        BattleText.enabled = false;
+        ToggleButton(BattleAttackButton, false);
+        ToggleButton(BattleFleeButton, false);
     }
 
-    public void StartBattle()
+    public void StartBattle(Sprite sprite, Action<string> action)
     {
+        _battleAction = action;
         BattleBackground.enabled = true;
+        BattleSprite.sprite = sprite;
+        BattleSprite.enabled = true;
+        BattleText.enabled = true;
+        ToggleButton(BattleAttackButton, true);
+        ToggleButton(BattleFleeButton, true);
     }
 
-    public void QuitBattle()
+    public void QuitBattle(string result)
     {
         BattleBackground.enabled = false;
+        BattleSprite.enabled = false;
+        BattleText.enabled = false;
+        ToggleButton(BattleAttackButton, false);
+        ToggleButton(BattleFleeButton, false);
+        _battleAction.Invoke(result);
+    }
+
+    private void ToggleButton(Button button, bool active)
+    {
+        button.enabled = active;
+        button.image.enabled = active;
+        button.GetComponentInChildren<Text>().enabled = active;
     }
 }
