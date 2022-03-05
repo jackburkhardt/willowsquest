fe890ecfd0e928b9dc69d03a28317f713f41d9c8
Jack Burkhardt <mail@jackburkhardt.com>
2022-03-05 12:59:56 -0600

    fix scripting with cobra

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,15 +1,14 @@
 ﻿public enum GameState
 {
      Start, // objective: talk to tutorial npcs
-     FightSquirrels, // objective: fight the squirrels to get the key back
-     Stump, // objective: get ket from stump, open gate
-     //FightFoxes, // objective: fight wolves that ambush you
-     CheckSecondGate,
-     MeetFrog, // objective: talk to small bear blocking path
-     FindWorms, // objective: find the honey hidden in a pile of pots/rubbish
-     GiveWorms, // objective: give honey to small bear
-     MeetCobra, // objective: talk to king cob
-     FightSkunks, // objective: fight the foxes for the crown
+     FightSquirrels, // objective: fight the squirrels to get the key location
+     Stump, // objective: get ket from bush, open gate
+     CheckSecondGate, // objective: walk over to the second gate (realize gatekeeper is asleep)
+     MeetFrog, // objective: talk to frog who may be able to help
+     FindWorms, // objective: frog has you find worms hidden under crates (requires pushing them around)
+     GiveWorms, // objective: give worms back to frog
+     MeetCobra, // objective: go thru gate, talk to king cobra
+     FightSkunks, // objective: fight the skunks for the crown
      GiveCrown, // objective: give crown to king cobra
      FightBear, // objective: defeat the bear
      Finish // objective: save ur friend
